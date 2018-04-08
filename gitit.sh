3aca16a3b59e7cc54d11828a4001ea972c2b9943
Patrick Haller <patrickhaller40@googlemail.com>
2018-04-08 02:28:02 +0200

    Update

---
+++ gitit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add .
+git commit -m "Update"
+git push -u origin master
--- lib/main.dart
+++ lib/main.dart
@@ -29,21 +29,43 @@ void outputFunction(String output) {
 
 String icWithoutArguments() {
   // 4th called frame is ic-Call
-  var stack = new DartStack();
-  var line =  stack.getFrame(4).lineNumber;  
-  var filename_dir = stack.getFrame(4).source;
-  var filename = filename_dir.split("/").last;
-  return "$filename:$line";
+  var stack = new DartStack().getFrame(4);
+
+  // Line number of 4th frame 
+  var lineNumber =  stack.lineNumber;  
+
+  // Take script name of complete directory
+  var filename = stack.source.split("/").last;
+
+  return "$filename:$lineNumber";
 }
 
 String icWithArguments(arguments) {
   String output = "";
-  for (final arg in arguments) {
+
+  var stack = new DartStack().getFrame(4);
+  
+  var lineNumber = stack.lineNumber;
+
+  var filename = stack.source.split("/").last;
+
+  for (var i=0; i < arguments.length; i++) {
+    getVariableName(filename, lineNumber, i);
+    var arg = arguments[i];
     final type = reflect(arg).type.reflectedType.toString();
     final name = reflect(arg).type.qualifiedName.toString();
     output += type + ": " + arg.toString() + ", ";
   }
   return output;
+}
+
+String getVariableName(String filename, int line, int argumentIndex) async {
+  var stack = new DartStack();
+  var lineNumber = stack.getFrame(6).lineNumber;
+  
+  List<String> lines = await new File(filename).readAsLines();
+  print(lines[lineNumber-1]);
+  return "";
 }
 
 class IcCreamDebugger {
@@ -52,12 +74,13 @@ class IcCreamDebugger {
 
   final call = new VarargsFunction((arguments) {
     var output;
+    // If no arguments are given, output filename and called line
     if (arguments.length == 0) {
       output = icWithoutArguments();
     } else {
       output = icWithArguments(arguments);
     }
-    
+
     outputFunction("$prefix $output ");
   });
 
